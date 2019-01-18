229b6a5253eb5de42ac1fbab64dc2bd87a32b6c4
Nala Mahen <mail2mahen@yahoo.co.uk>
2019-01-18 01:43:44 +0000

    Add config folder

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import keys from '../config/keys'
 
-const WEATHER_API_KEY = '3a94b715e87f028e09b0374ec1410d45';
-const WEATHER_ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${WEATHER_API_KEY}`;
+//const WEATHER_API_KEY = '3a94b715e87f028e09b0374ec1410d45';
+const WEATHER_ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${keys.weatherApiKey}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
